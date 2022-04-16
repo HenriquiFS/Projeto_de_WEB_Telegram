09b722f27071ab582684db6ebe402bc73fd94cc4
Henriqui Ferreira Segantini <80061115+HenriquiFS@users.noreply.github.com>
2022-04-16 19:59:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 var char_name_txt = document.querySelector('#char_name'),
     char_img = document.querySelector('#char_img'),
     request = new XMLHttpRequest(),
-    charName = 'luigi';
+    button = document.querySelector('#buscar_api'),
+    charName = null;
 
-request.open('GET', 'https://amiiboapi.com/api/amiibo/?name=' + charName, true)
+button.addEventListener('click', () => {
+  var input = document.querySelector('#input_api');
+  charName = input.value;
 
-request.onreadystatechange = function () {
-  if (request.readyState === 4 && request.status === 200) {
-    let resposta = JSON.parse(request.responseText)
-    console.log(resposta);
-    char_name_txt.innerHTML = resposta.amiibo[0].character;
-    char_img.src = resposta.amiibo[0].image
+  request.open('GET', 'https://amiiboapi.com/api/amiibo/?name=' + charName, true)
+
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      let resposta = JSON.parse(request.responseText)
+      console.log(resposta);
+      char_name_txt.innerHTML = resposta.amiibo[0].character;
+      char_img.src = resposta.amiibo[0].image;
+    }
   }
-}
+  
+  request.send()
 
-request.send()
+});
