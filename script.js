911311e97e354d5e50937adf034126536dc67214
Isabela Pirola <54108814+isapirola@users.noreply.github.com>
2022-04-26 22:49:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var char_name_txt = document.querySelector('#char_name'),
     char_img = document.querySelector('#char_img'),
     request = new XMLHttpRequest(),
     button = document.querySelector('#buscar_api'),
+    loginButton = document.querySelector('#login_button'),
+    confirmButton = document.querySelector('#confirm'),
     charName = null;
+
 
 button.addEventListener('click', () => {
   var input = document.querySelector('#input_api');
@@ -17,8 +20,26 @@ button.addEventListener('click', () => {
       char_name_txt.innerHTML = resposta.amiibo[0].character;
       char_img.src = resposta.amiibo[0].image;
     }
-  }
-  
+  }  
   request.send()
+});
 
+loginButton.addEventListener('click', () => {
+  document.querySelector('.login_box').style.display = 'block';
+});
+
+confirmButton.addEventListener('click', () => {
+  var loginEmail = document.querySelector('#login_email').value;
+  var loginSenha = document.querySelector('#login_senha').value;
+
+  axios.post('https://reqres.in/api/login',{
+    "email": document.querySelector('#login_email').value,
+    "password": document.querySelector('#login_senha').value
+  })
+  .then( () => {
+    document.querySelector('.container_busca').style.display = 'block';
+  })
+  .catch( () => {
+    alert("Erro!!!!");
+  });
 });
