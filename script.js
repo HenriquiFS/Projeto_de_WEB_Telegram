c4e33b65c5ca7776a444ab2da6558364f52f5ca9
Isabela Pirola <54108814+isapirola@users.noreply.github.com>
2022-04-29 22:57:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var char_name_txt = document.querySelector('#char_name'),
     loginButton = document.querySelector('#login_button'),
     confirmButton = document.querySelector('#confirm'),
     charName = null;
+    cookieEmail = null;
+    cookieSenha = null;
+
 
 
 button.addEventListener('click', () => {
@@ -28,18 +31,32 @@ loginButton.addEventListener('click', () => {
   document.querySelector('.login_box').style.display = 'block';
 });
 
-confirmButton.addEventListener('click', () => {
-  var loginEmail = document.querySelector('#login_email').value;
-  var loginSenha = document.querySelector('#login_senha').value;
-
+function fazer_login(auxEmail, auxSenha){
   axios.post('https://reqres.in/api/login',{
-    "email": document.querySelector('#login_email').value,
-    "password": document.querySelector('#login_senha').value
+    "email": auxEmail,
+    "password": auxSenha
   })
   .then( () => {
     document.querySelector('.container_busca').style.display = 'block';
+    document.querySelector('#login_button').innerHTML = 'Logout';
+    localStorage.setItem(cookieEmail, auxEmail);
+    localStorage.setItem(cookieSenha, auxSenha);
   })
   .catch( () => {
     alert("Erro!!!!");
   });
+}
+
+confirmButton.addEventListener('click', () => {
+  var loginEmail = document.querySelector('#login_email').value;
+  var loginSenha = document.querySelector('#login_senha').value;
+  fazer_login(loginEmail, loginSenha);
+
 });
+
+if(localStorage.getItem(cookieEmail) !== null && localStorage.getItem(cookieSenha) !== null){
+  var storageEmail = localStorage.getItem(cookieEmail);
+  var storageSenha = localStorage.getItem(cookieSenha);
+  fazer_login(storageEmail, storageSenha);
+}
+
